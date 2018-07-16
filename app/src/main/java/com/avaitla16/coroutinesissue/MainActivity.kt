8c3f99a496db902fbc6cf51950310af4fdf18210
Anil Vaitla <avaitla16@Anils-MacBook-Pro.local>
2018-07-16 17:00:50 -0500

    Basic Repro

--- app/src/main/java/com/avaitla16/coroutinesissue/MainActivity.kt
+++ app/src/main/java/com/avaitla16/coroutinesissue/MainActivity.kt
@@ -2,94 +2,12 @@ package com.avaitla16.coroutinesissue
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
-import awaitString
-import com.github.kittinunf.fuel.httpGet
-import kotlinx.coroutines.experimental.*
-import kotlinx.coroutines.experimental.android.UI
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val makeRequestBtn = findViewById<Button>(R.id.request_btn)
-
-        makeRequestBtn.setOnClickListener {
-            printRemoteDataV1()
-
-            printRemoteDataV2()
-
-            printRemoteDataV3()
-
-            printRemoteDataV4()
-        }
-
     }
-
-    fun toastMessage(string: String) {
-        Toast.makeText(this@MainActivity, string, Toast.LENGTH_LONG).show()
-    }
-
-
-
-    fun printRemoteDataV1() {
-        val job = async(CommonPool) { getDataFromWebV1() }
-
-        launch(UI) {
-            val result = job.await()
-            toastMessage("V1: $result")
-        }
-    }
-
-    suspend fun getDataFromWebV1(): String {
-        return "https://httpbin.org/uuid".httpGet().awaitString()
-    }
-
-
-
-    fun printRemoteDataV2() = launch(CommonPool) {
-        val result = getDataFromWebV2()
-
-        launch(UI) {
-            toastMessage("V2: $result")
-        }
-    }
-
-    suspend fun getDataFromWebV2(): String {
-        return "https://httpbin.org/uuid".httpGet().awaitString()
-    }
-
-
-
-    fun printRemoteDataV3() {
-        launch(UI) {
-            val result = getDataFromWebV3()
-            toastMessage("V3: $result")
-        }
-    }
-
-    suspend fun getDataFromWebV3(): String = withContext(CommonPool) {
-        "https://httpbin.org/uuid".httpGet().awaitString()
-    }
-
-
-
-    fun printRemoteDataV4() {
-        launch(UI) {
-            val result = getDataFromWebV4().await()
-            toastMessage("V4: $result")
-        }
-    }
-
-    fun getDataFromWebV4(): Deferred<String> {
-        return async(CommonPool) {
-            "https://httpbin.org/uuid".httpGet().awaitString()
-        }
-    }
-
-
-
 }
--- app/src/test/java/com/avaitla16/coroutinesissue/ExampleUnitTest.kt
+++ app/src/test/java/com/avaitla16/coroutinesissue/ExampleUnitTest.kt
@@ -1,17 +1,29 @@
 package com.avaitla16.coroutinesissue
 
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.core.DefaultEnvironment
+import com.github.kittinunf.fuel.core.FuelManager
 import org.junit.Test
 
-import org.junit.Assert.*
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
+    init {
+        // Required otherwise Fuel fails with 'Method getMainLooper in android.os.Looper not mocked'
+        FuelManager.instance.callbackExecutor = DefaultEnvironment().callbackExecutor
+        Fuel.testMode()
+    }
+
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        Fuel.get("https://www.httpbin.org/uuid").responseString { _, _, msg ->
+            println("Demonstrate Blocking by Sleeping...")
+            Thread.sleep(5000)
+            println("Woke from Sleeping...")
+
+            assert(true)
+            print("Assert True Passes")
+
+            assert(false)
+            print("Assert False Passes")
+        }
     }
 }
